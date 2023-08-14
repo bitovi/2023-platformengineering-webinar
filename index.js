48e13c2baa43c0a52baf2148784c289c00c0f0da
Mick McGrath <mick.mc.13@gmail.com>
2023-08-14 15:42:41 -0400

    update message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send("Hello World - v1");
+  res.send("Hello Platform Engineering Webinar!");
 });
 
 app.listen( port, function () {
